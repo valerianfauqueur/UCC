1add1fc82091c9de422036c36a8b321c2cb7aa4d
Alex Berneau <haldir.alex@gmail.com>
2016-04-15 12:54:49 +0200

    home side nav comment

--- src/js/controllers/archives.js
+++ src/js/controllers/archives.js
@@ -1,14 +1,3 @@
-// SCROLLING EFFECT WITH JQUERY
-$(document).ready(function() {
-
-		$('.scrollTo').click( function() {
-			var page = $(this).attr('href'); 
-			var speed = 600; // Animation duration
-			$('html, body').animate( { scrollTop: $(page).offset().top }, speed );
-			return false;
-		});
-	});
-
 // ZOOM EFFECT ON CLICK
 $(".test").click(function(){
 	$(this).toggleClass('zoom');
---
+++ src/js/controllers/home.js
@@ -0,0 +1,11 @@
+// SCROLLING EFFECT WITH JQUERY (not working properly with php routing)
+/*$(document).ready(function() {
+
+		$('.scrollTo').click( function() {
+			var page = $(this).attr('href'); 
+			var speed = 600; // Animation duration
+			$('html, body').animate( { scrollTop: $(page).offset().top }, speed );
+			return false;
+		});
+	});
+*/
