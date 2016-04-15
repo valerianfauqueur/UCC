4d7bdc3e02d23fac2908438be602e732208da874
Alex Berneau <haldir.alex@gmail.com>
2016-04-15 12:33:55 +0200

    Footer added

--- views/partials/footer.php
+++ views/partials/footer.php
@@ -1,5 +1,11 @@
-    <footer>
-
+    <footer id="footer">
+		<img class="tmdb-logo" src="src/img/tmdb.png" alt="">
+    	<p>This website uses the <a target="blank" href="https://www.themoviedb.org/documentation/api">TMDB API.</a> We do not own the content that they provide.</p>
+    	<p>You can also found this project on <a target="blank" href="https://github.com/Exotit/semaineBack">GitHub !</a></p>
+    	<a href="https://twitter.com/share" class="twitter-share-button" data-text="Go check UltimateCharacterChampionship, the best way to define which movie character is the best !" data-via="UCC_BOT" data-hashtags="UCC">Tweet</a>
+<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
+    	<a href="https://twitter.com/UCC_BOT" class="twitter-follow-button" data-show-count="default">Follow @UCC_BOT</a>
+<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
     </footer>
 
     <?php
